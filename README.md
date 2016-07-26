9f6b884e4abb16bb511f3d37025260fbb7a9fb51
Olexander T <timqha@gmail.com>
2016-07-26 21:25:42 +0300

    init project

---
+++ README.md
@@ -0,0 +1,11 @@
+#PORTFOLIO ROZELLO-TEAM
+
+##DEMO
+    Rozello-team.ltd.ua
+
+##Installation:
+    
+    bower install
+    npm install
+
+###Design by Stanislav Yaresko
---
+++ gulpfile.js
@@ -0,0 +1,125 @@
+'use strict';
+// generated on 2016-07-13 using generator-gulp-bootstrap 0.0.4
+
+var gulp = require('gulp');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+var uglify      = require('gulp-uglify');
+var gutil = require('gulp-util');
+
+// load plugins
+var $ = require('gulp-load-plugins')();
+
+gulp.task('styles', function () {
+    return gulp.src('app/styles/**/*.+(scss|sass) ')
+        .pipe(sourcemaps.init())
+        .pipe($.sass({errLogToConsole: true}))
+        .pipe($.autoprefixer('last 5 version'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('app/styles'))
+        .pipe(reload({stream:true}))
+        .pipe($.size())
+        .pipe($.notify("Compilation complete."));
+});
+
+gulp.task('scripts', function () {
+    return gulp.src('app/scripts/**/*.js')
+        .pipe($.jshint())
+        .pipe($.jshint.reporter(require('jshint-stylish')))
+        .pipe($.size());
+});
+
+gulp.task('html', ['styles', 'scripts'], function () {
+    var jsFilter = $.filter('**/*.js');
+    var cssFilter = $.filter('**/*.css');
+
+    return gulp.src('app/*.html')
+        .pipe($.useref.assets())
+        .pipe(jsFilter)
+        .pipe($.uglify())
+        .pipe(jsFilter.restore())
+        .pipe(cssFilter)
+        .pipe($.csso())
+        .pipe(cssFilter.restore())
+        .pipe($.useref.restore())
+        .pipe($.useref())
+        .pipe(gulp.dest('dist'))
+        .pipe($.size());
+});
+
+gulp.task('images', function () {
+    return gulp.src('app/images/**/*')
+        .pipe($.cache($.imagemin({
+            optimizationLevel: 3,
+            progressive: true,
+            interlaced: true
+        })))
+        .pipe(gulp.dest('dist/images'))
+        .pipe(reload({stream:true, once:true}))
+        .pipe($.size());
+});
+
+gulp.task('fonts', function () {
+    var streamqueue = require('streamqueue');
+    return streamqueue({objectMode: true},
+        $.bowerFiles(),
+        gulp.src(['app/fonts/**/*', 'bower_components/lato-font/fonts/**/*'])
+    )
+        .pipe($.filter('**/*.{eot,svg,ttf,woff}'))
+        .pipe($.flatten())
+        .pipe(gulp.dest('dist/fonts'))
+        .pipe($.size());
+});
+
+gulp.task('clean', function () {
+    return gulp.src(['app/styles/*.css', 'dist'], { read: false }).pipe($.clean());
+});
+
+gulp.task('build', ['html', 'images', 'fonts']);
+
+gulp.task('default', ['clean'], function () {
+    gulp.start('build');
+});
+
+gulp.task('serve', ['styles'], function () {
+    browserSync.init(null, {
+        server: {
+            baseDir: 'app',
+            directory: true
+        },
+        debugInfo: false,
+        open: false,
+        hostnameSuffix: ".xip.io"
+    }, function (err, bs) {
+        require('opn')(bs.options.get('urls').get('local'));
+        console.log('Started connect web server on ' + bs.options.url);
+    });
+});
+
+// inject bower components
+gulp.task('wiredep', function () {
+    var wiredep = require('wiredep').stream;
+    gulp.src('app/styles/*.scss')
+        .pipe(wiredep({
+            directory: 'app/bower_components'
+        }))
+        .pipe(gulp.dest('app/styles'));
+    gulp.src('app/*.html')
+        .pipe(wiredep({
+            directory: 'app/bower_components',
+            // exclude: ['bootstrap-sass-official']
+        }))
+        .pipe(gulp.dest('app'));
+});
+
+gulp.task('watch', ['serve'], function () {
+
+    // watch for changes
+    gulp.watch(['app/*.html'], reload);
+    gulp.watch('app/styles/**/*.scss', ['styles']);
+    gulp.watch('app/scripts/**/*.js', ['scripts']);
+    gulp.watch('app/images/**/*', ['images']);
+    gulp.watch('bower.json', ['wiredep']);
+});
